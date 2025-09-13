0a394471b522fd48794e5cca258bbe0c71425a44
douglascdev <douglasc.dev@gmail.com>
2025-09-12 21:56:58 -0300

    Transparent

--- init.lua
+++ init.lua
@@ -958,6 +958,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
