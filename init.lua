f23ac7f3816b2ea11115ce308cefb5f50b35a806
douglascdev <douglasc.dev@gmail.com>
2025-09-13 13:33:37 -0300

    go stuff

--- init.lua
+++ init.lua
@@ -538,14 +538,6 @@ require('lazy').setup({
     },
     config = function(lp, opts)
       require('go').setup(opts)
-      local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = '*.go',
-        callback = function()
-          require('go.format').goimports()
-        end,
-        group = format_sync_grp,
-      })
     end,
     event = { 'CmdlineEnter' },
     ft = { 'go', 'gomod' },
@@ -838,7 +830,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, go = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
