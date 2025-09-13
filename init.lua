c725236c13d053b07981e613c8d39bd152c756b6
douglascdev <douglasc.dev@gmail.com>
2025-09-13 11:49:41 -0300

    discord presence

--- init.lua
+++ init.lua
@@ -295,6 +295,8 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  'andweeb/presence.nvim',
+  'ThePrimeagen/vim-be-good',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
