0809a83e0430d5542242722e7d258ea09053c1f1
douglascdev <douglasc.dev@gmail.com>
2026-05-02 15:38:07 -0300

    Revert "set context window"
    This reverts commit 0fb2ab67e01425447e8a411d06bb116a0cb73358.

--- init.lua
+++ init.lua
@@ -275,14 +275,14 @@ require('lazy').setup({
     config = function()
       require('minuet').setup {
         provider = 'openai_fim_compatible',
-          request_timeout = 8000,
+          request_timeout = 10000,
           n_completions = 1, -- recommend for local model for resource saving
           -- I recommend beginning with a small context window size and incrementally
           -- expanding it, depending on your local computing power. A context window
           -- of 512, serves as an good starting point to estimate your computing
           -- power. Once you have a reliable estimate of your local computing power,
           -- you should adjust the context window to a larger value.
-          context_window = 4096,
+          context_window = 512,
           provider_options = {
             openai_fim_compatible = {
               -- For Windows users, TERM may not be present in environment variables.
@@ -292,7 +292,7 @@ require('lazy').setup({
               end_point = 'http://localhost:11434/v1/completions',
               model = 'qwen2.5-coder:7b',
               optional = {
-                  max_tokens = 64,
+                  max_tokens = 56,
                   top_p = 0.9,
               },
             },
